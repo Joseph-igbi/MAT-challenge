94b0e25e35ea3226ad72e7b0fb98ae93f457b44b
Ubuntu <ubuntu@ip-172-31-30-160.eu-west-2.compute.internal>
2020-12-06 22:15:39 +0000

    private host sg fixed

--- Infrastructure/terraform-environment/user_data_bastion.sh
+++ Infrastructure/terraform-environment/user_data_bastion.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install ansible -y
 sudo -u ubuntu bash -c "cd /home/ubuntu; git clone --branch bastion https://github.com/Joseph-igbi/MAT-challenge.git" 
-cd /home/ubuntu/ansible
+cd /home/ubuntu/MAT-challenge
 ansible-playbook playbook.yaml
 
 
--- Infrastructure/terraform-environment/vpc.tf
+++ Infrastructure/terraform-environment/vpc.tf
@@ -127,6 +127,14 @@ resource "aws_security_group" "sgph" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   vpc_id = aws_vpc.the_vpc.id
 
   tags = {
