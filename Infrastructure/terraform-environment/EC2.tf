92de9b18d0f2f298201f77e0a79c6cd48507e36b
Ubuntu <ubuntu@ip-172-31-30-160.eu-west-2.compute.internal>
2020-12-06 10:43:57 +0000

    ansible and terraform added

---
+++ Infrastructure/terraform-environment/EC2.tf
@@ -0,0 +1,30 @@
+
+resource "aws_instance" "bastion_host" {
+  ami                         = var.ami
+  instance_type               = "t2.micro"
+  key_name                    = var.key_name
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.sgbh.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "bastionhost"
+  }
+}
+
+resource "aws_instance" "private_host" {
+  ami                    = var.ami
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  subnet_id              = aws_subnet.private_subnet.id
+  vpc_security_group_ids = [aws_security_group.sgph.id]
+
+  tags = {
+    Name = "private_host"
+  }
+}
+
+
+
+
+
