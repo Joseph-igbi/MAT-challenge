a28db78e0d51d5948bf2a5d0088a9d5c52dbd36f
Ubuntu <ubuntu@ip-172-31-30-160.eu-west-2.compute.internal>
2020-12-06 11:19:32 +0000

    aws cli added to ansible

--- Infrastructure/terraform-environment/vpc.tf
+++ Infrastructure/terraform-environment/vpc.tf
@@ -114,20 +114,20 @@ resource "aws_security_group" "sgph" {
     protocol    = "tcp"
     cidr_blocks = [var.public_subnet_cidr]
   }
-   ingress {
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- ingress {
+  ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-    egress {
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
