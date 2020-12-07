62d6d2f1b321410ddea6ea1e20aae9ca51dee15a
Ubuntu <ubuntu@ip-172-31-30-160.eu-west-2.compute.internal>
2020-12-07 16:10:11 +0000

    ebs-volume

--- Infrastructure/terraform-environment/user_data_private.sh
+++ Infrastructure/terraform-environment/user_data_private.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo apt update
 sudo apt install ansible -y
+sudo mkfs -t xfs /dev/sdd
+sudo mkdir /data
+sudo mount /dev/sdd /data
 sudo -u ubuntu bash -c "cd /home/ubuntu; git clone https://github.com/Joseph-igbi/MAT-challenge.git" 
 cd /home/ubuntu/MAT-challenge/ansible 
 ansible-playbook playbook.yaml
